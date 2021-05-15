ee695c915df30fb04f83901313e09aaaedc74177
Darya Loskutova <dariy.dece@gmail.com>
2021-05-15 18:25:50 +0300

    add cal fun

--- app/src/main/java/com/example/tipcalculator/MainActivity.kt
+++ app/src/main/java/com/example/tipcalculator/MainActivity.kt
@@ -2,12 +2,40 @@ package com.example.tipcalculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.tipcalculator.databinding.ActivityMainBinding
+import java.text.NumberFormat
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        binding.buttonCalculate.setOnClickListener {
+            if (binding.editService.text.isEmpty())
+                Toast.makeText(this, "Enter price...", Toast.LENGTH_SHORT).show()
+            else
+                calculateTip()
+        }
+    }
 
+    private fun calculateTip() {
+        val price = binding.editService.text.toString().toDouble()
+        val tipPercentage = when(binding.radioGroup.checkedRadioButtonId) {
+            R.id.radio_amazing -> 0.20
+            R.id.radio_good -> 0.15
+            else -> 0.1
+        }
+        var tip = tipPercentage * price
+        if (binding.switchRound.isChecked) {
+            tip = kotlin.math.ceil(tip)
+        }
+        val tipStr = NumberFormat.getCurrencyInstance().format(tip)
+        binding.textResult.text = tipStr
     }
 }
+
